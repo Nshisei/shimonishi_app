823099164dd2284791e4b3256819707baa816a97
Nshisei <n.shisei1215@gmail.com>
2023-07-05 17:56:22 +0900

    connection

--- connection/target.py
+++ connection/target.py
@@ -1,11 +1,8 @@
 import socket
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-
-from get_temp import readSensor
 # from get_temp import readSensor
-target_ip = "192.168.4.1"
+target_ip = "192.168.4.3"
 target_port = 3334
 buffer_size = 1024
 
@@ -13,5 +10,4 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((target_ip, target_port))
 response = s.recv(buffer_size)
 print(response.decode("utf-8"))
-
 s.close()
