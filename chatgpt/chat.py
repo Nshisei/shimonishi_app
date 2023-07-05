628c37d124b4e2697f10ae11fbecd41f040eb734
mtanaka1177 <u710058f@ecs.osaka-u.ac.jp>
2023-07-05 16:35:13 +0900

    07.05

--- chatgpt/chat.py
+++ chatgpt/chat.py
@@ -1,13 +1,48 @@
 import openai
-import sys
+import os
+from dotenv import load_dotenv
 
-temp = sys.argv[1]
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": "気温が{}度の時の服を60文字で".format(temp)},
-    ]   
-)
 
-print(response['choices'][0]['message']['content'])
+def get_temperature():
+    temp = "20"
+    return temp
+
+def chatgpt(temperature):
+    openai.api_key = os.environ["API_KEY"]
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        # engine="davinci",
+        # prompt="気温が{}度の時の服を60文字で".format(temperature)
+        messages=[
+            {"role": "user", "content": "気温が{}度の時の服を60文字で".format(temperature)},
+        ]   
+    )
+
+    return response['choices'][0]['message']['content']
+
+
+
+load_dotenv()
+
+# Install the Slack app and get xoxb- token in advance
+app = App(token=os.environ["SLACK_BOT_TOKEN"])
+
+@app.event("app_mention")
+def message_mention(say):
+    temperature = get_temperature()
+    advice = chatgpt(temperature)
+    say(advice)
+
+@app.event("message")
+def handle_message_events(body, logger):
+    logger.info(body)
+
+
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+
+
+
