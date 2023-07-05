be6872c8770e3705ce461c55f68f933465ee102d
Nshisei <n.shisei1215@gmail.com>
2023-07-05 17:15:10 +0900

    get_temp

--- connection/target.py
+++ connection/target.py
@@ -1,8 +1,21 @@
 import socket
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from get_temp import readSensor
+# from get_temp import readSensor
 target_ip = "192.168.4.1"
 target_port = 3334
 buffer_size = 1024
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((target_ip, target_port))
-messsage = s.recv(1024)
-print(messsage.decode("utf-8"))
+
+message = str(readSensor())  # 送信するメッセージ文字列
+s.send(message.encode("utf-8"))  # メッセージをエンコードして送信
+
+response = s.recv(buffer_size)
+print(response.decode("utf-8"))
+
+s.close()
---
+++ get_temp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# Note:
+# ds18b20's data pin must be connected to pin7.
+# Reads temperature from sensor and prints to stdout
+# id is the id of the sensor
+import os
+import time
+def readSensor():
+    for file in os.listdir("/sys/bus/w1/devices/"):
+        if not (file.startswith("28-")):
+            continue
+        tfile = open("/sys/bus/w1/devices/"+file+"/w1_slave")
+        text = tfile.read()
+        tfile.close()
+        secondline = text.split("\n")[1]
+        temperaturedata = secondline.split(" ")[9]
+        temperature = float(temperaturedata[2:])
+        temperature = temperature / 1000
+        return temperature
+
+
+# Reads temperature from all sensors found in /sys/bus/w1/devices/
+# starting with "28-.."
+
+if __name__ == "__main__":
+    readSensor()
+
