123cf6afaad9cf55ddfb4bec49b009235ae7036d
Nshisei <n.shisei1215@gmail.com>
2023-07-26 10:30:27 +0900

    DB操作

--- post_database/db_operation.py
+++ post_database/db_operation.py
@@ -6,31 +6,80 @@ db_config = {
 	'port': '5432',
 	'user': 'postgres',
 	'database': 'db_clothes'}
-SQL = """
-    SELECT DISTINCT
-        shape
-        , color
-        , season
-    FROM
-        namelist n
-    LEFT JOIN
-        clothelist l
-    ON
-        n.id = l.person_id
-    WHERE
-        n.name = '<NAME>'
-    ;
-    """
+
 def load_query_to_dataframe(sql):
-	with psycopg2.connect(**db_config) as conn:
-		df = pd.read_sql(sql,conn).to_numpy().tolist()
-		return df
+    with psycopg2.connect(**db_config) as conn:
+        df = pd.read_sql(sql,conn)
+        return df
+
+
+def exec_sql_cmd(sql):
+    with psycopg2.connect(**db_config) as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql)
 
 def get_info_from_db(name):
-    sql = SQL.replace('<NAME>', name)
-    result = load_query_to_dataframe(sql)
+    with open('/home/suita/shimonishi_app/post_database/sql/get_clothes.sql') as f:
+        sql = f.read()
+    sql = sql.replace('<NAME>', name)
+    result = load_query_to_dataframe(sql).to_numpy().tolist()
     return result
 
+def get_gender(name):
+    with open('/home/suita/shimonishi_app/post_database/sql/get_users.sql') as f:
+        sql = f.read()
+    sql = sql.replace('<NAME>', name)
+    result = load_query_to_dataframe(sql)['gender'].iloc[0]
+    return result
+
+def get_all_data():
+    with open('/home/suita/shimonishi_app/post_database/sql/get_all_data.sql') as f:
+            sql = f.read()
+    result = load_query_to_dataframe(sql).to_string()
+    return result
+
+def insert_clothes(person_id, color, shape, season):
+    try:
+        id = get_all_data().count('\n') + 1
+        print(id, person_id, color, shape, season)
+        sql = f"insert into clothelist1 values ({id}, {person_id}, '{color}','{shape}', '{season}');"
+        exec_sql_cmd(sql)
+        return
+    except Exception as e:
+        return e
+
+def delete_clothes(id):
+    try:
+        sql = f"delete from clothelist1 where id = {id};"
+        exec_sql_cmd(sql)
+        return
+    except Exception as e:
+        return e
+
+def insert_user(id, name, gender):
+    # try:
+    sql = f"insert into namelist1 values ({id}, '{name}', '{gender}');"
+    exec_sql_cmd(sql)
+    return
+    # except Exception as e:
+    #     return e
+
+def delete_user(id):
+    try:
+        sql = f"delete from namelist1 where id = {id};"
+        exec_sql_cmd(sql)
+        return
+    except Exception as e:
+        return e
+
 if __name__ == '__main__':
-    name = 'Bushi' 
-    print(get_info_from_db(name))
+    # insert_user(0, 'test','w')
+    # print(get_all_data())
+    delete_user(0)
+    print(get_all_data())
+    # delete_clothes('12')
+    # delete_clothes('14')
+    # insert_clothes('23026','白','シャツ','夏')
+    # print(get_all_data())
+    # print(get_gender('Bushi'))
+    # print(get_all_data())
--- slack.py
+++ slack.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from connection.target import get_temp_from_server
-from post_database.db_operation import get_info_from_db
+from post_database.db_operation import *
 from chatgpt.chat import *
 
 
@@ -14,16 +14,95 @@ app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
 # @app.event("app_mention")
 @app.message('服装予報')
-def message_mention(message, say):
-    name = message['text'].replace('服装予報', '').strip()  # 名前抽出
-    print(name)
-    temperature = get_temperature()  # get_温度
-    clothes_list = get_info_from_db(name)
-    text = make_text(temperature, clothes_list)
-    print(text)
-    advice = chatgpt(text)  # chatgptにメッセージを投げて、返してもらう
-    print(advice)
-    say(advice)
+def _message_mention(message, say):
+    text = message['text']
+    if '服装予報' in text:
+        f_message = False
+        if '服装予報 with list' in text:
+            name = text.replace('服装予報 with list', '').strip()  # 名前抽出
+            f_message = True
+        else:
+            name = text.replace('服装予報', '').strip()  # 名前抽出
+        print(name)
+        temperature = get_temperature()  # get_温度
+        gender = get_gender(name)
+        clothes_list = get_info_from_db(name)
+        text = make_text(temperature, clothes_list, gender)
+        print(text)
+        message = chatgpt(text)  # chatgptにメッセージを投げて、返してもらう
+        print(message)
+        if f_message:
+            message = text + '\n\n' + message
+    say(message)
+
+@app.message('DB参照')
+def _message_mention_db(message, say):
+    text = message['text']
+    if 'DB参照' in text:
+        print(text)
+        all_data = get_all_data()
+        message = all_data
+    say(message)
+
+@app.message('insert clothes')
+def _insert_clothes(message, say):
+    text = message['text']
+    if 'insert clothes' in text:
+        print(text)
+        value = text.replace('insert clothes', '').strip()  # 名前抽出
+        value_list = value.split(',')
+        text = insert_clothes(*value_list)
+        if text != '':
+            all_data = get_all_data()
+            message = all_data
+        else:
+            message = text
+    say(message)
+
+@app.message('delete clothes')
+def _delete_clothes(message, say):
+    text = message['text']
+    if 'delete clothes' in text:
+        print(text)
+        id = text.replace('delete clothes', '').strip()  # 名前抽出
+        text = delete_clothes(id)
+        if text != '':
+            all_data = get_all_data()
+            message = all_data
+        else:
+            message = text
+    say(message)
+
+    
+@app.message('insert user')
+def _insert_user(message, say):
+    text = message['text']
+    if 'insert user' in text:
+        print(text)
+        value = text.replace('insert user', '').strip()  # 名前抽出
+        value_list = value.split(',')
+        text = insert_user(*value_list)
+        if text != '':
+            all_data = get_all_data()
+            message = all_data
+        else:
+            message = text
+    say(message)
+
+
+@app.message('delete user')
+def _delete_user(message, say):
+    text = message['text']
+    if 'delete user' in text:
+        print(text)
+        id = text.replace('delete user', '').strip()  # 名前抽出
+        text = delete_user(id)
+        if text != '':
+            all_data = get_all_data()
+            message = all_data
+        else:
+            message = text
+    say(message)
 
 @app.event("message")
 def handle_message_events(body, logger):
